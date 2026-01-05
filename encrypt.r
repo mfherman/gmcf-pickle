d25648f626dff40c9c0238a98c4a637709f65182
Matt Herman <mherman@csg.org>
2026-01-05 16:47:28 -0500

    remove encrypt

--- encrypt.r
+++
@@ -1,8 +0,0 @@
-staticryptR::staticryptr(
-  files = "_site/index.html",
-  directory = "_site",
-  password = "pickleparty",
-  short = TRUE,
-  template_title = "GMCF Pickleball Utilization",
-  template_button = "Submit"
-)
