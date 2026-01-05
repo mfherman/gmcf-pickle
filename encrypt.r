1c82e5ca8a17684094e5fc928ace31bab3a23ccb
Matt Herman <mherman@csg.org>
2026-01-04 20:28:57 -0500

    add password

---
+++ encrypt.r
@@ -0,0 +1,8 @@
+staticryptR::staticryptr(
+  files = "_site/index.html",
+  directory = "_site",
+  password = "pickleparty",
+  short = TRUE,
+  template_title = "GMCF Pickleball Utilization",
+  template_button = "Submit"
+)
